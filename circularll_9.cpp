c42928c9c1703aeed7a8d37e32d82a889515038b
shantanu <56212958+cigar-galaxy82@users.noreply.github.com>
2022-05-28 17:04:14 +0530

    Create circularll_9.cpp

---
+++ circularll_9.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    struct Node *next;
+};
+
+Node *first;
+
+void create(int A[],int n) {
+    Node *temp, *last;
+    first = new Node;
+    first->data= A[0];
+    first->next = first;
+    last = first;
+
+    for(int i=1;i<n;i++) {
+        temp = new Node;
+        temp->data = A[i];
+        temp->next = last->next;
+        last->next = temp;
+        last = temp;
+    }
+}
+
+void display(Node *p) {
+    do{
+        cout<<p->data<<" ";
+        p = p->next;
+    }while(p != first);
+}
+
+int main()
+{
+    int A[] = {6,2,10,40,1};
+    create(A,5);
+    display(first);
+}
